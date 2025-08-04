18fb11fa963466ccac2503b9c6dc34f13f3cd325
unknown <Administrador@capir-267341.ifpi.int>
2025-08-04 08:05:52 -0300

    last commit

--- run.py
+++ run.py
@@ -37,10 +37,11 @@ def main():
     print()
     
     try:
+        port = int(os.environ.get("PORT", 10000))
         socketio.run(
-            app, 
-            host='0.0.0.0', 
-            port=5000, 
+            app,
+            host='0.0.0.0',
+            port=port,
             debug=False,
             allow_unsafe_werkzeug=True
         )
